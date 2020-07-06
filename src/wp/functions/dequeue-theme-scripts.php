cc260ef081427224ec65a0709f73c9bfd200f161
Reigo Tuulik <rtuulik@gmail.com>
2020-07-06 23:21:06 +0300

    replcae scripts by WP

---
+++ src/wp/functions/dequeue-theme-scripts.php
@@ -0,0 +1,23 @@
+<?php
+
+function reigo_dequeue_theme_scripts(){
+	wp_dequeue_script( '_s-navigation' );	// remove link to /js/navigation.js in functions.php
+	wp_dequeue_style( '_s-style' );
+}
+add_action( 'wp_enqueue_scripts', 'reigo_dequeue_theme_scripts', 100 );
+
+function reigo_enqueue_theme_scripts(){
+	wp_enqueue_script( 'main.min.js', get_template_directory_uri() . '/dist/js/main.min.js', array(), null, true );	// concatinated and minified main.min.js
+	wp_enqueue_style( 'main.min.css', get_template_directory_uri() . '/dist/css/main.min.css', array(), null, 'all');
+}
+add_action( 'wp_enqueue_scripts', 'reigo_enqueue_theme_scripts', 100 );
+
+
+// saad kasutada seda ja pass-ida siia versiooninumbri asemel cache buster hashi 
+// wp_enqueue_script( 'script-name', get_template_directory_uri() . '/dist/js/main.min.js', array(), '1.0.0', true );
+// wp_enqueue_style( 'main.min.css', get_template_directory_uri() . '/dist/css/main.min.css', array(), '1.1', 'all');	
+
+/* 
+ * remove link to woocomerrce.css if selection is false 
+ * 
+ * */
