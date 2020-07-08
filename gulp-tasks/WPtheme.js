f832f98b6d61529960b572ea14849d1a175a8774
Reigo <rtuulik@gmail.com>
2020-07-08 17:24:13 +0300

    remove addFunctions & separate dev & src

--- gulp-tasks/WPtheme.js
+++ gulp-tasks/WPtheme.js
@@ -9,17 +9,19 @@ const settings = {
 	images: false,
 	fonts: false,
 	bustCache: false,
-	//	reload: true,
-
-	wooCommerse: undefined, // kas lisada/eemaldada Woo css ja faili templated (pluss hiljem ka muud asjad, product galeriid jms)
-	wordpress: true, // use to configure wordpress build options(found under gulpfile-wp.js)
+	reload: undefined,
 };
 
-const settingsWPFunctions = {
-	disableEmbeds: true,
-	disableEmoji: true,
-	redirectAttachmentPages: true,
-	removeComments: true,
+const WPsettings = {
+	wordpress: true, // use to configure wordpress build options(found under gulpfile-wp.js)
+	woocommerse: false, // incude/not all wooCommerce files to theme build or not
+
+	functions: {
+		disableEmbeds: true,
+		disableEmoji: true,
+		redirectAttachmentPages: true,
+		removeComments: false,
+	},
 };
 
 /* const settingsWPStyles = {
@@ -55,9 +57,9 @@ const filePaths = {
 		output: "./",
 
 		moveSassinput: "src/wp/themes/_s/sass/**/*.scss",
-		moveSassOutput: "src/dev/sass/underscores",
-		sassFiles: "src/dev/underscores/**/*.scss",
-		allSassFiles: "./src/dev/**/*.scss",
+		moveSassOutput: "dev/sass/underscores",
+		sassFiles: "dev/underscores/**/*.scss",
+		allSassFiles: "./dev/**/*.scss",
 		moveJSinput: "src/wp/themes/_s/js/**/*.js",
 		moveJSOutput: "js/", // "src/dev/js/",
 	},
@@ -89,8 +91,7 @@ const terser = require("gulp-terser"); // minifies JS. gulp-uglify doesn´t supp
 
 // Sass task: compiles the sass files into one main style.min.css
 const buildStyles = function (done) {
-	// Make sure this feature is activated before running
-	if (!settings.styles) return done();
+	if (!settings.styles) return done(); // Make sure this feature is activated before running
 
 	return src(filePaths.underscoresTheme.allSassFiles) // filePaths.underscoresTheme.moveSassOutput
 		.pipe(sourcemaps.init()) // sourcemaps needs to be added first after src()
@@ -104,8 +105,7 @@ const buildStyles = function (done) {
 
 // JS task: concatenates and minifies JS files to script.js
 const buildScripts = function (done) {
-	// Make sure this feature is activated before running
-	if (!settings.scripts) return done();
+	if (!settings.scripts) return done(); // Make sure this feature is activated before running
 
 	return src(filePaths.scripts.input) // load the JS files from files.jsPath
 		.pipe(concat("main.js")) // concatenate all the JS files into one JS file
@@ -116,16 +116,13 @@ const buildScripts = function (done) {
 
 // move images to the build folder
 const moveImages = function (done) {
-	// Make sure this feature is activated before running
-	if (!settings.images) return done();
-
+	if (!settings.images) return done(); // Make sure this feature is activated before running
 	return src(filePaths.images.input).pipe(dest(filePaths.images.output));
 };
 
 // move fonts to the build folder
 const moveFonts = function (done) {
 	if (!settings.fonts) return done();
-
 	return src(filePaths.fonts.input).pipe(dest(filePaths.fonts.output));
 };
 
@@ -141,19 +138,14 @@ const bustCache = function (done) {
 
 // Remove pre-existing content from output folders
 const cleanDist = function (done) {
-	// Make sure this feature is activated before running
-	if (!settings.clean) return done();
-
-	// Clean the dist folder
-	del.sync([filePaths.output]);
-
-	// Signal completion
-	return done();
+	if (!settings.clean) return done(); // Make sure this feature is activated before running
+	del.sync([filePaths.output]); // Clean the dist folder
+	return done(); // Signal completion
 };
 
 // Take fresh copy of a theme, and pipe it to root folder
 const compileTheme = function (done) {
-	if (!settings.wordpress) return done();
+	if (!WPsettings.wordpress) return done();
 
 	// move _s sass files from theme root to src/dev/sass
 	src(filePaths.underscoresTheme.moveSassinput).pipe(dest(filePaths.underscoresTheme.moveSassOutput));
@@ -162,53 +154,69 @@ const compileTheme = function (done) {
 	src(filePaths.underscoresTheme.moveJSinput).pipe(dest(filePaths.underscoresTheme.moveJSOutput));
 
 	// create sass & js folders in dev folder, and create basic structure for users sass & JS files
-	fs.existsSync("src/dev/") || fs.mkdirSync("src/dev/"); // create folders first, otherwise the whole process fails
-	fs.existsSync("src/dev/js/") || fs.mkdirSync("src/dev/js/");
-	fs.existsSync("src/dev/sass/") || fs.mkdirSync("src/dev/sass/");
-	fs.existsSync("src/dev/sass/user") || fs.mkdirSync("src/dev/sass/user");
-	fs.existsSync("src/dev/fonts/") || fs.mkdirSync("src/dev/fonts/");
-	fs.existsSync("src/dev/img/") || fs.mkdirSync("src/dev/img/");
+	fs.existsSync("dev/") || fs.mkdirSync("dev/"); // create folders first, otherwise the whole process fails
+	fs.existsSync("dev/js/") || fs.mkdirSync("dev/js/");
+	fs.existsSync("dev/sass/") || fs.mkdirSync("dev/sass/");
+	fs.existsSync("dev/sass/user") || fs.mkdirSync("dev/sass/user");
+	fs.existsSync("dev/fonts/") || fs.mkdirSync("dev/fonts/");
+	fs.existsSync("dev/img/") || fs.mkdirSync("dev/img/");
 
-	fs.writeFileSync("src/dev/js/shame.js", "// place to put all the temporary things you promise to fix \n"); // create shame.js
-	fs.writeFileSync("src/dev/sass/user/_navigation.scss", "");
+	fs.writeFileSync("dev/js/shame.js", "// place to put all the temporary things you promise to fix \n"); // create shame.js
+	fs.writeFileSync("dev/sass/user/_navigation.scss", "");
 	/* 	fs.writeFileSync("src/dev/sass/user/_typography.scss", "");
 	fs.writeFileSync("src/dev/sass/user/_colors.scss", ""); */
-	fs.writeFileSync("src/dev/sass/user/_shame.scss", "");
-	fs.writeFileSync("src/dev/sass/user/style.scss", '@import "_navigation.scss"; \n@import "_shame.scss"; \n');
+	fs.writeFileSync("dev/sass/user/_shame.scss", "");
+	fs.writeFileSync("dev/sass/user/style.scss", '@import "_navigation.scss"; \n@import "_shame.scss"; \n');
 
 	// move "_s" theme files to root folder. This way the theme is recognizable to the WordPress install
 	src(filePaths.underscoresTheme.input).pipe(dest(filePaths.underscoresTheme.output));
 
+	const disableEmbeds = function () {
+		if (!WPsettings.functions.disableEmbeds) return done();
+		src("functions.php").pipe(footer("\nrequire get_template_directory() . '/inc/disable-embeds.php';")).pipe(dest("."));
+		return src("src/wp/functions/disable-embeds/disable-embeds.php").pipe(dest("inc/"));
+	};
+
+	const disableEmoji = function () {
+		if (!WPsettings.functions.disableEmoji) return done();
+		src("functions.php").pipe(footer("\nrequire get_template_directory() . '/inc/disable-emoji.php';")).pipe(dest("."));
+		return src("src/wp/functions/disable-emoji/disable-emoji.php").pipe(dest("inc/"));
+	};
+
+	const redirectAttachmentPages = function () {
+		if (!WPsettings.functions.redirectAttachmentPages) return done();
+		src("functions.php").pipe(footer("\nrequire get_template_directory() . '/inc/redirect-attachment-pages-to-parent.php';")).pipe(dest("."));
+		return src("src/wp/functions/redirect-attachment-pages/redirect-attachment-pages-to-parent.php").pipe(dest("inc/"));
+	};
+
+	const removeComments = function () {
+		if (!WPsettings.functions.removeComments) return done();
+		src("functions.php").pipe(footer("\nrequire get_template_directory() . '/inc/remove-comments.php';")).pipe(dest("."));
+		return src("src/wp/functions/remove-comments/remove-comments.php").pipe(dest("inc/"));
+	};
+
+	const deququeScripts = function () {
+		src("functions.php").pipe(footer("\nrequire get_template_directory() . '/inc/dequeue-theme-scripts.php';")).pipe(dest("."));
+		return src("src/wp/functions/dequeue-theme-scripts.php").pipe(dest("inc/"));
+	};
+
+	disableEmbeds();
+	disableEmoji();
+	redirectAttachmentPages();
+	removeComments();
+	deququeScripts();
+
 	return done();
 };
 
-const disableEmbeds = function (done) {
-	if (!settingsWPFunctions.disableEmbeds) return done();
-	src("functions.php").pipe(footer("\nrequire get_template_directory() . '/inc/disable-embeds.php';")).pipe(dest("."));
-	return src("src/wp/functions/disable-embeds/disable-embeds.php").pipe(dest("inc/"));
-};
+// WooCOmmerce settings
+const wooBuild = function (done) {
+	if (!WPsettings.woocommerse) return done();
 
-const disableEmoji = function (done) {
-	if (!settingsWPFunctions.disableEmoji) return done();
-	src("functions.php").pipe(footer("\nrequire get_template_directory() . '/inc/disable-emoji.php';")).pipe(dest("."));
-	return src("src/wp/functions/disable-emoji/disable-emoji.php").pipe(dest("inc/"));
-};
+	console.log("WOOOOOOOOOOO");
 
-const redirectAttachmentPages = function (done) {
-	if (!settingsWPFunctions.redirectAttachmentPages) return done();
-	src("functions.php").pipe(footer("\nrequire get_template_directory() . '/inc/redirect-attachment-pages-to-parent.php';")).pipe(dest("."));
-	return src("src/wp/functions/redirect-attachment-pages/redirect-attachment-pages-to-parent.php").pipe(dest("inc/"));
-};
-
-const removeComments = function (done) {
-	if (!settingsWPFunctions.removeComments) return done();
-	src("functions.php").pipe(footer("\nrequire get_template_directory() . '/inc/remove-comments.php';")).pipe(dest("."));
-	return src("src/wp/functions/remove-comments/remove-comments.php").pipe(dest("inc/"));
-};
-
-const deququeScripts = function (done) {
-	src("functions.php").pipe(footer("\nrequire get_template_directory() . '/inc/dequeue-theme-scripts.php';")).pipe(dest("."));
-	return src("src/wp/functions/dequeue-theme-scripts.php").pipe(dest("inc/"));
+	// del all woo css files to
+	return done();
 };
 
 // Watch task: watch SCSS and JS files for changes
@@ -220,18 +228,19 @@ const watchFiles = function (done) {
 	);
 };
 
+// exports[taskName] = taskFunction;
 // Export the default Gulp task so it can be run automatically run if you type in gulp on the command line
 exports.default = series(parallel(cleanDist, buildStyles, buildScripts, moveImages, moveFonts), bustCache, watchFiles); // Runs the tasks simultaneously and then runs bustCache, then watch task
 exports.compileTheme = series(compileTheme);
 
 // NB! can´t call
-exports.addFunctions = series(disableEmbeds, disableEmoji, redirectAttachmentPages, removeComments, deququeScripts); // move all needed files to functions.php
+//exports.addFunctions = series(disableEmbeds, disableEmoji, redirectAttachmentPages, removeComments, deququeScripts); // move all needed files to functions.php
 
 /* 
 *
 * playing around with renaming things inside the files
 exports.buildStyles = buildStyles;
-//exports.compileTheme = series('compileTheme', 'coffee')
+//exports.theme = series('theme', 'coffee')
 exports.coffee = coffee;
 
 const coffee = function (done) {
